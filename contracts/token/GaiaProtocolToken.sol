f4540cb0e1c73f0d7824be8c11c09cbe50c1e157
yj.gaia <gaiaprotocolnft@gmail.com>
2025-07-31 23:13:50 +0900

    Update GaiaProtocolToken.sol

--- contracts/token/GaiaProtocolToken.sol
+++ contracts/token/GaiaProtocolToken.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract GaiaProtocolToken is ERC20Permit {
+    // Initial supply: 100 000 000 tokens, 18 decimals
+    uint256 private constant INITIAL_SUPPLY = 100_000_000 * 10 ** 18;
+
     constructor() ERC20("Gaia Protocol", "GAIA") ERC20Permit("Gaia Protocol") {
-        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
